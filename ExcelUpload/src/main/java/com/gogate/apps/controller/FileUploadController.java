a728c2862d07fad5fecc41348bdcf1095016b8c4
Gaurav Gogate <gaurav.gogate@gmail.com>
2017-03-26 23:01:41 +0530

    initial commit

---
+++ ExcelUpload/src/main/java/com/gogate/apps/controller/FileUploadController.java
@@ -0,0 +1,69 @@
+package com.gogate.apps.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.gogate.apps.util.ExcelReader;
+import com.gogate.apps.projects.*;
+
+/**
+ * Handles requests for the application file upload requests
+ */
+@Controller
+public class FileUploadController {
+	
+	@Autowired
+	ExcelReader reader;
+	
+	private static final Logger logger = LoggerFactory
+			.getLogger(FileUploadController.class);
+
+	/**
+	 * Upload single file using Spring Controller
+	 */
+	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+	public @ResponseBody String uploadFileHandler(@RequestParam("file") MultipartFile file) {
+		
+		if (!file.isEmpty()) {
+			try {
+				byte[] bytes = file.getBytes();
+
+				// Creating the directory to store file
+				String rootPath = System.getProperty("catalina.home");
+				File dir = new File(rootPath + File.separator + "tmpFiles");
+				if (!dir.exists())
+					dir.mkdirs();
+
+				// Create the file on server
+				File serverFile = new File(dir.getAbsolutePath()
+						+ File.separator + file.getOriginalFilename());
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(serverFile));
+				stream.write(bytes);
+				stream.close();
+
+				logger.info("Server File Location="	+ serverFile.getAbsolutePath());
+				//new ExcelReader().readExcel(serverFile);
+				reader.readExcel(serverFile);
+				return "You successfully uploaded file=" + file.getOriginalFilename();
+			} catch (Exception e) {
+				return "You failed to upload " + file.getOriginalFilename() + " => " + e.getMessage();
+			}
+		} else {
+			return "You failed to upload " + file.getOriginalFilename()
+					+ " because the file was empty.";
+		}
+	}
+
+}
